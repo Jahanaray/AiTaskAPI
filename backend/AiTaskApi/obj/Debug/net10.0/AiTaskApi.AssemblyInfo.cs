4a5dd2e51d2a64cd12e77d98193ccd7f82e67d08
Pro <roeen.dej@outlook.com>
2026-06-16 21:53:13 +0330

    Start of CI/CD

--- backend/AiTaskApi/obj/Debug/net10.0/AiTaskApi.AssemblyInfo.cs
+++ backend/AiTaskApi/obj/Debug/net10.0/AiTaskApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AiTaskApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c75f99eded6a7b257b349c453b21d11c21cf352")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+404047ee73685aae19405b187fb7034c65479c0d")]
 [assembly: System.Reflection.AssemblyProductAttribute("AiTaskApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AiTaskApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
